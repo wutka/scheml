5a8639dc274e3f1e2cfea766ff5529dfcff041e2
Mark Wutka <mark@wutka.com>
2021-06-07 20:54:42 -0500

    Added type checking for printf by parsing the format string

--- src/main/java/edu/vanderbilt/cs/wutkam/scheme/expr/PrintfExpr.java
+++ src/main/java/edu/vanderbilt/cs/wutkam/scheme/expr/PrintfExpr.java
@@ -2,31 +2,36 @@ package edu.vanderbilt.cs.wutkam.scheme.expr;
 
 import edu.vanderbilt.cs.wutkam.scheme.LispException;
 import edu.vanderbilt.cs.wutkam.scheme.runtime.Environment;
-import edu.vanderbilt.cs.wutkam.scheme.type.StringType;
-import edu.vanderbilt.cs.wutkam.scheme.type.TypeRef;
-import edu.vanderbilt.cs.wutkam.scheme.type.UnifyException;
-import edu.vanderbilt.cs.wutkam.scheme.type.VoidType;
+import edu.vanderbilt.cs.wutkam.scheme.type.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** Provides a wrapper for Java System.out.printf/String.format allowing for multiple, varied arguments
- * The format strings are those specified by java.util.Formatter.
+ * The format strings are those specified by java.util.Formatter. Type checking is performed by parsing
+ * the format string and matching against argument types.
  */
 public class PrintfExpr implements Expression {
     String formatString;
     boolean returnString;
     List<Expression> parameters;
+    List<StringFormatTypeRef> formatStringTypes;
 
-    public PrintfExpr(boolean returnString, String formatString, List<Expression> parameters) {
+    public PrintfExpr(boolean returnString, String formatString, List<Expression> parameters)
+        throws LispException {
         this.returnString = returnString;
         this.formatString = formatString;
+        this.formatStringTypes = parseFormatString(formatString);
         this.parameters = parameters;
-
     }
     
     @Override
     public Expression evaluate(Environment<Expression> env, boolean inTailPosition) throws LispException {
+
+        // Evaluate the parameters
         Object[] args = new Object[parameters.size()];
         for (int i=0; i < args.length; i++) {
             args[i] = parameters.get(i).evaluate(env, false).toJavaValue();
@@ -45,10 +50,37 @@ public class PrintfExpr implements Expression {
 
     @Override
     public void unify(TypeRef typeRef, Environment<TypeRef> env) throws LispException {
+        // initialize the flags to make sure each arg is accounted for in the format string
+        boolean[] unifiedArg = new boolean[parameters.size()];
+        for (int i=0; i < unifiedArg.length; i++) unifiedArg[i] = false;
 
-        // We don't unify on the parameters. Theoretically we could, by examining the format string, but that
-        // is left as a future exercise.
-        
+        // Unify on the type parameters
+        for (StringFormatTypeRef formatTypeRef: formatStringTypes) {
+            if ((formatTypeRef.argNumber < 0) || (formatTypeRef.argNumber >= parameters.size())) {
+                throw new LispException("Format string refers to argument number "+(formatTypeRef.argNumber+1)+
+                        " but there are only "+parameters.size()+" parameters");
+            }
+            TypeRef argType = formatTypeRef.typeRef.copy(new HashMap<>());
+            try {
+                // Unify the parameter with what is required by the format type
+                parameters.get(formatTypeRef.argNumber).unify(argType, env);
+
+                // Mark this arg as having been unified
+                unifiedArg[formatTypeRef.argNumber] = true;
+            } catch (UnifyException exc) {
+                throw UnifyException.addCause("Error unifying arg "+(formatTypeRef.argNumber+1)+
+                        " with type "+argType.getType()+" in format string "+formatString, exc);
+            }
+        }
+
+        // Make sure all the args have been unified
+        for (int i=0; i < unifiedArg.length; i++) {
+            if (!unifiedArg[i]) {
+                throw new UnifyException("Format string arg "+(i+1)+" is not referenced in format string "+
+                        formatString);
+            }
+        }
+
         // This expression returns a string if returnString is true, otherwise a void
         if (returnString) {
             typeRef.unify(new TypeRef(StringType.TYPE));
@@ -57,44 +89,75 @@ public class PrintfExpr implements Expression {
         }
     }
 
-    static List<StringFormatTypeRef> parseFormatString(String formatString) {
-        List<StringFormatTypeRef> formatTypeRefs = new ArrayList<>();
+    // Define a regex to match the java formatter format string, ignoring the date formats
+    static final String formatRegex = "%(?<argPos>[<0-9]+[$])?(?<argLastPos><)?(?<flags>[-#+ 0,(]*)(?<width>[0-9]+)?(?<precision>[.][0-9]+)?(?<conversion>.)";
+    static final Pattern formatPattern = Pattern.compile(formatRegex);
 
-        int pos = 0;
-        int length = formatString.length();
-        while (pos < length) {
-            char ch = formatString.charAt(pos++);
-            if (ch != '%') continue;
+    public static List<StringFormatTypeRef> parseFormatString(String formatString)
+        throws LispException {
 
-            if (pos >= length) return formatTypeRefs;
+        List<StringFormatTypeRef> formatTypes = new ArrayList<>();
 
-            ch = formatString.charAt(pos++);
-            int argPosition = -1;
-            int width = -1;
-            int precision = -1;
-            if (Character.isDigit(ch)) {
-                int num = ch - '0';
-                while ((pos < length) && Character.isDigit(formatString.charAt(pos))) {
-                    ch = formatString.charAt(pos++);
-                    num = 10 * num + ch - '0';
+        // Parse the format
+        Matcher matcher = formatPattern.matcher(formatString);
+        int positionalArgNumber = 0;
+        int lastArgNumber = 0;
+
+        // For each format string found...
+        while (matcher.find()) {
+            // Get the parameters we are interested in
+            String argPos = matcher.group("argPos");
+            String argLastPos = matcher.group("argLastPos");
+            String conversion = matcher.group("conversion");
+
+            int argNumber;
+
+            if ((argPos != null) && (argPos.length() > 1)) {
+                // If the argPos was specified, strip off the $ at the end and parse it as an int
+                argPos = argPos.substring(0, argPos.length() - 1);
+                try {
+                    // Subtract 1 since the format position numbers start from 1
+                    argNumber = Integer.parseInt(argPos) - 1;
+                } catch (NumberFormatException exc) {
+                    throw new LispException("Unable to parse arg number " + argPos + " in format string " + formatString);
                 }
-                if (pos >= length) return formatTypeRefs;
-                if (formatString.charAt(pos) == '$') {
-                    argPosition = num;
-                } else if (formatString.charAt(pos) == '.') {
-                    width = num;
-                    pos++;
-                    if (pos >= length) return formatTypeRefs;
-                    num = 0
-                    while ((pos < length) && Character.isDigit(formatString.charAt(pos))) {
-                        ch = formatString.charAt(pos++);
-                        num = num * 10 + ch - '0';
-                    }
-                }
+                // Note the last arg number used
+                lastArgNumber = argNumber;
+            } else if ((argLastPos != null) && (argLastPos.length() > 0)) {
+                // If the argLastPos (<) was specified, use lastArgNumber
+                argNumber = lastArgNumber;
+            } else {
+                // Otherwise use the positional arg count
+                lastArgNumber = positionalArgNumber;
+                argNumber = positionalArgNumber++;
+            }
+
+            // If the conversion is a % it's not a conversion
+            if (conversion.equals("%")) continue;
+
+            switch (conversion) {
+                case "B", "H", "S", "b", "h", "s":
+                    formatTypes.add(new StringFormatTypeRef(new TypeRef(), argNumber));
+                    break;
+                case "C", "c":
+                    formatTypes.add(new StringFormatTypeRef(new TypeRef(CharType.TYPE), argNumber));
+                    break;
+                case "d", "o", "x", "X":
+                    formatTypes.add(new StringFormatTypeRef(new TypeRef(IntType.TYPE), argNumber));
+                    break;
+                case "e", "E", "f", "g", "G", "a", "A":
+                    formatTypes.add(new StringFormatTypeRef(new TypeRef(DoubleType.TYPE), argNumber));
+                    break;
+                default:
+                    // Don't do anything for conversions we don't recognize
+                    break;
             }
         }
+
+        return formatTypes;
     }
-    static class StringFormatTypeRef {
+
+    public static class StringFormatTypeRef {
         public TypeRef typeRef;
         public int argNumber;
 
---
+++ src/test/java/edu/vanderbilt/cs/wutkam/scheme/expr/TestFormatParser.java
@@ -0,0 +1,19 @@
+package edu.vanderbilt.cs.wutkam.scheme.expr;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TestFormatParser {
+    @Test
+    public void testFormatParser() {
+        try {
+            List<PrintfExpr.StringFormatTypeRef> refs = PrintfExpr.parseFormatString("%d %s %b %07d %1.3f %% %n %0d");
+            System.out.println();
+            refs = PrintfExpr.parseFormatString("%d %2$s %<$b %07d %<$1.3f %% %n %0d");
+            System.out.println();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
