6eeb954a244f1e97bab74ad1b764613c3d346fcd
Mark Wutka <mark@wutka.com>
2021-06-12 11:52:18 -0500

    Added built-in implementations of length and nth, but not actually making them available

---
+++ src/main/java/edu/vanderbilt/cs/wutkam/scheme/expr/builtin/Length.java
@@ -0,0 +1,28 @@
+package edu.vanderbilt.cs.wutkam.scheme.expr.builtin;
+
+import edu.vanderbilt.cs.wutkam.scheme.LispException;
+import edu.vanderbilt.cs.wutkam.scheme.expr.AbstractTypeExpr;
+import edu.vanderbilt.cs.wutkam.scheme.expr.Expression;
+import edu.vanderbilt.cs.wutkam.scheme.expr.IntExpr;
+
+/** A function to find the nth element of a list
+ */
+public class Length extends BuiltinFunctionExpr {
+    public Length(String name) {
+        super(name, "cons 'a -> 'a");
+    }
+
+    @Override
+    public Expression executeBuiltin(Expression[] args) throws LispException {
+        // We don't don't check the types here, the type unification will take
+        // care of that because of the function signature
+        AbstractTypeExpr curr = (AbstractTypeExpr) args[0];
+        long len = 0;
+        while (!curr.constructorName.equals("Nil")) {
+            curr = (AbstractTypeExpr) curr.values.get(1);
+            len++;
+        }
+
+        return new IntExpr(len);
+    }
+}
---
+++ src/main/java/edu/vanderbilt/cs/wutkam/scheme/expr/builtin/Nth.java
@@ -0,0 +1,35 @@
+package edu.vanderbilt.cs.wutkam.scheme.expr.builtin;
+
+import edu.vanderbilt.cs.wutkam.scheme.LispException;
+import edu.vanderbilt.cs.wutkam.scheme.expr.AbstractTypeExpr;
+import edu.vanderbilt.cs.wutkam.scheme.expr.Expression;
+import edu.vanderbilt.cs.wutkam.scheme.expr.IntExpr;
+import edu.vanderbilt.cs.wutkam.scheme.type.builtin.ConsTypeDecl;
+
+/** A function to find the nth element of a list
+ */
+public class Nth extends BuiltinFunctionExpr {
+    public Nth(String name) {
+        super(name, "int -> cons 'a -> 'a");
+    }
+
+    @Override
+    public Expression executeBuiltin(Expression[] args) throws LispException {
+        // We don't don't check the types here, the type unification will take
+        // care of that because of the function signature
+        long n = ((IntExpr) args[0]).value;
+        AbstractTypeExpr curr = (AbstractTypeExpr) args[1];
+        while (n > 0) {
+            if (curr.constructorName.equals("Nil")) {
+                throw new FailException("Ran out of elements looking for nth element");
+            }
+            curr = (AbstractTypeExpr) curr.values.get(1);
+            n--;
+        }
+        if (curr.constructorName.equals("Nil")) {
+            throw new FailException("Ran out of elements looking for nth element");
+        }
+
+        return curr.values.get(0);
+    }
+}
