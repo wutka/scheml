5d23936b84950de9e0666a9f7fffa1958e4894af
Mark Wutka <mark@wutka.com>
2021-06-13 11:14:20 -0500

    Added do-not-explode-assign to tcotest to make sure it still works for the assign form

--- tcotest.scm
+++ tcotest.scm
@@ -15,6 +15,13 @@
                    (do-not-explode-1 (- n 1) (+ 1 acc))))))
     (do-not-explode-1 n 0)))
 
+(define (do-not-explode-assign n)
+  (:= do-not-explode-1
+      (lambda (n acc)
+        (if (<= n 0) acc
+            (do-not-explode-1 (- n 1) (+ 1 acc)))))
+  (do-not-explode-1 n 0))
+
 ;; Dummy definition to allow even to refer to odd
 (define (odd n) #f)
 (define (even n) (if (= n 0) #t (odd (- n 1))))
